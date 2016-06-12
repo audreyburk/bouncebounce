12a4af1fdfc03335ba901eba774c6c40d6224318
Audrey Burk <audrey.jo.burk@gmail.com>
2016-06-12 15:25:10 -0700

    halfway through acceleration fixes

--- ball.js
+++ ball.js
@@ -29,7 +29,7 @@ Ball.makeBalls = function(canvas, n) {
       dx: Math.cos(angle * (Math.PI / 180)) * velocity,
       dy: Math.sin(angle * (Math.PI / 180)) * velocity,
       r: 20,
-      color: (i % 2 === 0 ? "red" : "blue")
+      color: (i % 2 === 0 ? "blue" : "blue")
     });
     balls.push(ball);
   }
