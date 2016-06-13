4f88145c63af462a3d2d2e6e6d10a28c90626094
Audrey Burk <audrey.jo.burk@gmail.com>
2016-06-12 18:09:16 -0700

    WIP updating basics

--- canvas.js
+++ canvas.js
@@ -1,6 +1,7 @@
 function Canvas(){
   let canvas = document.getElementById("myCanvas")
   this.ctx = canvas.getContext("2d");
+  this.ctx.globalAlpha = 0.7;
   this.width = canvas.width;
   this.height = canvas.height;
 }
