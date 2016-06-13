d1d09c579e6142a0ac5ada1141255cf6f76b270b
jordan <jordan.g.burk@gmail.com>
2016-06-12 18:38:33 -0700

    stable

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ function Game() {
   this.player = new Player(this.canvas);
   this.balls = Ball.makeBalls(this.canvas, 30);
 
+  this.startTime = Date.now();
+  this.elapsedTime = 0;
+
   this.running = true;
 }
 
@@ -17,6 +20,8 @@ Game.init = function(){
 
 Game.prototype.run = function () {
   this.draw();
+  this.changeTime();
+  this.showTime();
   this.player.accelerate();
   this.player.move(this.canvas);
   this.balls.forEach(ball => ball.move(this.canvas));
@@ -45,8 +50,7 @@ Game.prototype.checkCollisions = function() {
 Game.prototype.hitPlayer = function(num) {
   ball = this.balls[num];
   if (ball.color === "red"){
-    document.getElementById("title").textContent = "You lose.";
-    this.running = false;
+    this.lose();
   } else if (ball.color === "blue"){
     delete this.balls[num];
     this.player.r += 3;
@@ -59,11 +63,24 @@ Game.prototype.draw = function() {
   this.balls.forEach(ball => ball.draw(this.canvas));
 };
 
+Game.prototype.changeTime = function () {
+  this.elapsedTime = Math.floor((Date.now() - this.startTime)/1000);
+};
 
+Game.prototype.showTime = function () {
+  const displayTime = Game.stringifyTime(this.elapsedTime);
+  document.getElementById("time").textContent = displayTime;
+};
 
-// score by average size
-// assuming we shrink over time
+Game.stringifyTime = function(time){
+  const minutes = Math.floor(time/60);
+  const seconds = time - minutes * 60;
+  const minutesStr = ("0" + minutes).slice(-2);
+  const secondsStr = ("0" + seconds).slice(-2);
+  return `${minutesStr}:${secondsStr}`;
+}
 
-// function bounce(a, b) {
-//   // maaaaaaaaaaaaaath
-// }
+Game.prototype.lose = function () {
+  document.getElementById("time").style.color = "red";
+  this.running = false;
+};
